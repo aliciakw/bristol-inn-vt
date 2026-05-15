e0853d8632f5adb29728f817415cc98d46c949b2
Alicia Willett <alicia@aleeshza.com>
2026-05-15 15:41:15 -0400

    chore: configure sentry (#4)
    * chore: configure sentry
    
    * fix: use env var

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_SENTRY_DSN: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+      }),
+      PUBLIC_GA4_ID: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+      }),
+      HOSTAWAY_API_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      PRISMIC_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      SENTRY_AUTH_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: true,
+      }),
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-  outDir: './dist',
-  server: { host: '127.0.0.1', port: 4321 }
+  outDir: "./dist",
+  server: { host: "127.0.0.1", port: 4321 },
+  integrations: [
+    sentry({
+      project: "bristol-inn-vt",
+      org: "aleeshza-llc",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 });
