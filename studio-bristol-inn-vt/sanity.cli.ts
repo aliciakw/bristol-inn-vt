38bc92591d2852c6a4619f8a855e57ee5a740bf7
Alicia Willett <alicia@aleeshza.com>
2026-06-29 10:12:04 -0400

    chore: ci task to deploy studio (#15)

--- studio-bristol-inn-vt/sanity.cli.ts
+++ studio-bristol-inn-vt/sanity.cli.ts
@@ -11,5 +11,10 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    /**
+     * Fixed hostname so `sanity deploy` is non-interactive (required for CI).
+     * Studio is served at https://bristol-inn-vt.sanity.studio
+     */
+     appId: 'uohr1he29r48yf25fvkvcfj5'
   }
 })
