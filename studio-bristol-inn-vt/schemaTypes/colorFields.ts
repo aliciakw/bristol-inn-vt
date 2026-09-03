c68b1a96a9233a9611ce97461207281488adbda7
Alicia Willett <alicia@aleeshza.com>
2026-09-02 22:02:20 -0400

    feat: announcement bar (#31)
    * feat: announcement bar
    
    * chore: remove .wrangler files
    
    * chore: restore .gitignore
    
    * feat: make announcement bar sticky
    
    * fix: bot comments

--- studio-bristol-inn-vt/schemaTypes/colorFields.ts
+++ studio-bristol-inn-vt/schemaTypes/colorFields.ts
@@ -29,21 +29,6 @@ export function defineTextColorField(
 }
 
 export const colorFields = [
-  // defineColorField(),
   defineBackgroundColorField(),
   defineTextColorField(),
-  // defineField({
-  //   name: 'backgroundColor',
-  //   title: 'Background Color Override',
-  //   description: 'Only include if you want to override the default background color for this block.',
-  //   type: 'string',
-  //   components: { input: ColorSwatchInput },
-  // }),
-  // defineField({
-  //   name: 'textColor',
-  //   title: 'Text Color Override',
-  //   description: 'Only include if you want to override the default text color for this block.',
-  //   type: 'string',
-  //   components: { input: ColorSwatchInput },
-  // }),
 ];
